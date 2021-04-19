c684ab33240399069083dfd429d9de2d70ff5903
Sergio Abad <saaideveloper@gmail.com>
2021-04-19 09:48:14 +0100

    First Version Empty Install Data

---
+++ Setup/InstallData.php
@@ -0,0 +1,16 @@
+<?php
+namespace WCS\CustomProductAttribute\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface; 
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface {
+	private $eavSetupFactory;
+
+	public function __construct(EavSetupFactory $eavSetupFactory) {
+		$this->eavSetupFactory = $eavSetupFactory; 
+	}
+}
